ebcf42b5c084adc9a63790fb6caf3f081ee5db0e
MathiasVeraM <mathias.vera@udla.edu.ec>
2023-05-22 16:05:53 -0500

    Primer intento de hacer la matriz transpuesta

--- MatrizTranspuesta.c
+++ MatrizTranspuesta.c
@@ -20,6 +20,11 @@ int main(){
         }
         printf("\n");
     }
-    
+    for(i=0;i<x;i++){
+        for(j=0;j<y;j++){
+            printf("%d ", Matriz[j][i]);
+        }
+        printf("\n");
+    }
     return 0;
 }
