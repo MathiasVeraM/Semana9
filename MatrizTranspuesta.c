82f800d6b0bf63a36adaad20b81ad0aa42d733ff
MathiasVeraM <mathias.vera@udla.edu.ec>
2023-05-22 15:54:11 -0500

    Indicaciones del programa

---
+++ MatrizTranspuesta.c
@@ -0,0 +1,9 @@
+//Generar un programa que defina una matriz con dimensiones ingresadas por el usuario, y la llene de numeros random de 0 a 100
+//Luego de generar esa matriz, calcula su transpuesta
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(){
+    srand(time(NULL));
+    return 0;
+}
